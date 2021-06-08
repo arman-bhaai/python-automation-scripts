ea1fdadb922bc9a18cf844a8746361278ccb98b0
Abu Bakar Siddique Arman <arman.bhaai@gmail.com>
2021-06-09 01:51:09 +0600

    updated #003 :: random update --> (v0.0.2)

--- 003_bulk_renamer.py
+++ 003_bulk_renamer.py
@@ -22,8 +22,8 @@
 #  \/                         bit.ly/arman-bhaai-on-youtube                                                                \/
 #  /\                                                                                                                      /\
 # //\\         Creation Date: 2021-06-05                                                                                  //\\
-# \\//               Version: v0.0.1                                                                                      \\//
-#  \/        Versioning Date: 2021-06-05                                                                                   \/
+# \\//               Version: v0.0.2                                                                                      \\//
+#  \/        Versioning Date: 2021-06-09                                                                                   \/
 #  /\                                                                                                                      /\
 # //\\               License: Custom License                                                                              //\\
 # \\//                                                                                                                    \\//
@@ -82,10 +82,14 @@ def err(msg):
     logging.error(msg)
 
 ############### CLI Configs ###############
-aparser = argparse.ArgumentParser()
+ap_desc = """\
+sample usage: python 003_bulk_renamer.py -b plus -ipg 5 -p bio1 -c 12
+"""
+aparser = argparse.ArgumentParser(description=ap_desc)
 aparser.add_argument(
-    'init_page_count',
+    '-ipg', '--init_page_count',
     type=int,
+    required=True,
     help='Resume page count'
 )
 aparser.add_argument(
@@ -95,12 +99,12 @@ aparser.add_argument(
     help='specify the name pattern of the files you want to rename'
 )
 aparser.add_argument(
-    '-p', '--paper_name',
+    '-p', '--paper_code',
     default='bio1',
     help='eg: bio1'
 )
 aparser.add_argument(
-    '-c', '--chp_name',
+    '-c', '--chp_no',
     default='ch1',
     help='specify the chapter name. \
             eg: ch1'
@@ -110,24 +114,38 @@ aparser.add_argument(
     default='jpg',
     help='eg: jpg'
 )
+aparser.add_argument(
+    '-b', '--book',
+    required=True,
+    help='eg: plus/royal/tbook'
+)
 args = aparser.parse_args()
-###########################################
+
+
+
+############### Variables ###############
+init_page_count = args.init_page_count
+glob_pattern = args.glob_pattern
+book = args.book
+paper_code = args. paper_code
+chp_no = args.chp_no
+file_ext = args.file_extension
 
 
 new_name_prefix = f'pg{str(args.init_page_count).zfill(3)}'
 
 p = pathlib.Path()
-fnames = p.glob(args.glob_pattern)
-fnames_check = p.glob(args.glob_pattern)
+fnames = p.glob(glob_pattern)
+fnames_check = p.glob(glob_pattern)
 if next(fnames_check, None) is None:
     err('No match found! Please input glob pattern correctly.')
     sys.exit()
 
 total_rename_count = 1
 for fname in fnames:
-    new_path_name = f'{args.paper_name}_pg{str(args.init_page_count).zfill(3)}_{args.chp_name}.{args.file_extension}'
+    new_path_name = f'{book}_{str(init_page_count).zfill(4)}_{paper_code}_ch{str(chp_no).zfill(2)}.{file_ext}'
     inf(f'Renaming: "{fname}" >> "{new_path_name}"')
     os.rename(fname, new_path_name)
-    args.init_page_count += 1
+    init_page_count += 1
     total_rename_count +=1
-inf(f'Total Renamed Files: {total_rename_count}')
+inf(f'Total Renamed Files: {total_rename_count-1}')
